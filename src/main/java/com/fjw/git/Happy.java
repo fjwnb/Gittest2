36b4acb48ef63b13868ad403a72b84e26b42ac5c
fjw_glb <fjw_glb@qq.com>
2018-08-28 18:06:18 +0800

    ssss

--- src/main/java/com/fjw/git/Happy.java
+++ src/main/java/com/fjw/git/Happy.java
@@ -3,7 +3,7 @@ package com.fjw.git;
 public class Happy {
 	private static void main(String[] args) {
 		System.out.println("reft...");
-		System.out.println("right...");
+		System.out.println("right...ssss");
 
 	}
 }
